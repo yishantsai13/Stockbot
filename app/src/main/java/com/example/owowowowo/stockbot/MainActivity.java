55dc3c9b7a3467b60336fb287e00eb95fd699cad
yishantsai <gary38p@gmail.com>
2018-04-10 16:42:34 +0800

    loginpage, mainpage

--- app/src/main/java/com/example/owowowowo/stockbot/MainActivity.java
+++ app/src/main/java/com/example/owowowowo/stockbot/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.owowowowo.stockbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEnter = findViewById(R.id.btn_enter);
+        btnEnter.setOnClickListener(btnEnterOnClick);
     }
+
+    private View.OnClickListener btnEnterOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent it = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(it);
+        }
+    };
 }
