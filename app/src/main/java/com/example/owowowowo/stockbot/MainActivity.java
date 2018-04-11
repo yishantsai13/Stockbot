33acd2d02459525f8bc9cfb7113df971d0f579cc
yishantsai <gary38p@gmail.com>
2018-04-11 22:47:19 +0800

    mainpage modify

--- app/src/main/java/com/example/owowowowo/stockbot/MainActivity.java
+++ app/src/main/java/com/example/owowowowo/stockbot/MainActivity.java
@@ -5,24 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnEnter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnEnter = findViewById(R.id.btn_enter);
-        btnEnter.setOnClickListener(btnEnterOnClick);
+        handler.postDelayed(runnable,2000);
     }
-
-    private View.OnClickListener btnEnterOnClick = new View.OnClickListener() {
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
         @Override
-        public void onClick(View view) {
-            Intent it = new Intent(MainActivity.this, Main2Activity.class);
-            startActivity(it);
+        public void run() {
+            startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            finish();
         }
     };
+
 }
